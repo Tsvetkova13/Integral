b0f1da58a0c37e905ad9a810b60558dac8da1ae7
AnaSihvart <ana.sihvart@gmail.com>
2020-03-20 23:04:01 +0300

    added something

--- integralTest/Count.cs
+++ integralTest/Count.cs
@@ -10,13 +10,11 @@ namespace integralTest
     {
         public void Rectangles(double a, double b, double h, double f)
         {
-            /* double F(double x)
+            double F(double x)
              {
-                 return 31 * x - log(5 * x) + 5;
-             }*/
+				return 31 * x - Math.Log(5 * x) + 5;
+			}
 
-
-            //double a, b, h;
             a= Convert.ToDouble(textBoxA.Text);
             b= Convert.ToDouble(textBoxB.Text);
             f = Convert.ToDouble(textFunc.Text); 
@@ -25,20 +23,44 @@ namespace integralTest
 
             double s = 0;
             float k = 0.5f;
+			
+			double  N = (b - a) / h;
+			a += h * k;
+			for (int i = 0; i < N; i++)
+			{
+				double x = a + h * i;
+				s += F(x);
+			}
 
-            int N = (b - a) / h;
-            a += h * k;
-            for (int i = 0; i < N; i++)
-                s += f(a + h * i);
+			textBoxRec.Text = s.ToString();
 
-        }
+		}
 
         public void Simpson(double a, double b, double h, double f)
         {
-            a = Convert.ToDouble(textBoxA.Text);
-            b = Convert.ToDouble(textBoxB.Text);
-            f = Convert.ToDouble(textFunc.Text); 
-            h = Convert.ToDouble(textBoxH.Text);
-        }
+			
+			a = Convert.ToDouble(textBoxA.Text);
+			b = Convert.ToDouble(textBoxB.Text);
+			f = Convert.ToDouble(textFunc.Text);
+			h = Convert.ToDouble(textBoxH.Text);
+
+			double N = (b - a) / h;
+
+				double F(double x)
+				{
+					return 31 * x - Math.Log(5 * x) + 5;
+				}
+
+				double k1 = 0, k2 = 0;
+				for (int i = 1; i < N; i += 2)
+				{
+					k1 += F(a + i * h);
+					k2 += F(a + (i + 1) * h);
+				}
+				double s1=h / 3 * (F(a) + 4 * k1 + 2 * k2);
+
+			textBoxSim.Text = s1.ToString();
+
+		}
     }
 }
--- integralTest/Form1.Designer.cs
+++ integralTest/Form1.Designer.cs
@@ -28,146 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-<<<<<<< HEAD
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBoxA = new System.Windows.Forms.TextBox();
-            this.labelA = new System.Windows.Forms.Label();
-            this.labelB = new System.Windows.Forms.Label();
-            this.textBoxB = new System.Windows.Forms.TextBox();
-            this.labelh = new System.Windows.Forms.Label();
-            this.textBoxH = new System.Windows.Forms.TextBox();
-            this.textFunc = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.Black;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.ForeColor = System.Drawing.Color.Red;
-            this.button1.Location = new System.Drawing.Point(465, 133);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(127, 63);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Count";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // textBoxA
-            // 
-            this.textBoxA.BackColor = System.Drawing.Color.Black;
-            this.textBoxA.ForeColor = System.Drawing.Color.Red;
-            this.textBoxA.Location = new System.Drawing.Point(225, 133);
-            this.textBoxA.Name = "textBoxA";
-            this.textBoxA.Size = new System.Drawing.Size(100, 20);
-            this.textBoxA.TabIndex = 1;
-            // 
-            // labelA
-            // 
-            this.labelA.AutoSize = true;
-            this.labelA.BackColor = System.Drawing.Color.Black;
-            this.labelA.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelA.ForeColor = System.Drawing.Color.Red;
-            this.labelA.Location = new System.Drawing.Point(61, 133);
-            this.labelA.Name = "labelA";
-            this.labelA.Size = new System.Drawing.Size(117, 39);
-            this.labelA.TabIndex = 2;
-            this.labelA.Text = "Low A";
-            // 
-            // labelB
-            // 
-            this.labelB.AutoSize = true;
-            this.labelB.BackColor = System.Drawing.Color.Black;
-            this.labelB.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelB.ForeColor = System.Drawing.Color.Red;
-            this.labelB.Location = new System.Drawing.Point(77, 193);
-            this.labelB.Name = "labelB";
-            this.labelB.Size = new System.Drawing.Size(101, 39);
-            this.labelB.TabIndex = 3;
-            this.labelB.Text = "UP B";
-            // 
-            // textBoxB
-            // 
-            this.textBoxB.BackColor = System.Drawing.Color.Black;
-            this.textBoxB.ForeColor = System.Drawing.Color.Red;
-            this.textBoxB.Location = new System.Drawing.Point(225, 193);
-            this.textBoxB.Name = "textBoxB";
-            this.textBoxB.Size = new System.Drawing.Size(100, 20);
-            this.textBoxB.TabIndex = 4;
-            // 
-            // labelh
-            // 
-            this.labelh.AutoSize = true;
-            this.labelh.BackColor = System.Drawing.Color.Black;
-            this.labelh.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelh.ForeColor = System.Drawing.Color.Red;
-            this.labelh.Location = new System.Drawing.Point(141, 247);
-            this.labelh.Name = "labelh";
-            this.labelh.Size = new System.Drawing.Size(37, 39);
-            this.labelh.TabIndex = 5;
-            this.labelh.Text = "h";
-            // 
-            // textBoxH
-            // 
-            this.textBoxH.BackColor = System.Drawing.Color.Black;
-            this.textBoxH.ForeColor = System.Drawing.Color.Red;
-            this.textBoxH.Location = new System.Drawing.Point(225, 247);
-            this.textBoxH.Name = "textBoxH";
-            this.textBoxH.Size = new System.Drawing.Size(100, 20);
-            this.textBoxH.TabIndex = 6;
-            // 
-            // textFunc
-            // 
-            this.textFunc.BackColor = System.Drawing.Color.Black;
-            this.textFunc.ForeColor = System.Drawing.Color.Red;
-            this.textFunc.Location = new System.Drawing.Point(225, 73);
-            this.textFunc.Name = "textFunc";
-            this.textFunc.Size = new System.Drawing.Size(100, 20);
-            this.textFunc.TabIndex = 7;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Black;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(48, 73);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(130, 37);
-            this.label1.TabIndex = 8;
-            this.label1.Text = "Fuction";
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.BackgroundImage = global::integralTest.Properties.Resources.IMG_2109;
-            this.ClientSize = new System.Drawing.Size(798, 368);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textFunc);
-            this.Controls.Add(this.textBoxH);
-            this.Controls.Add(this.labelh);
-            this.Controls.Add(this.textBoxB);
-            this.Controls.Add(this.labelB);
-            this.Controls.Add(this.labelA);
-            this.Controls.Add(this.textBoxA);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Tsvetkova, Shepeleva 3-46";
-            this.ResumeLayout(false);
-            this.PerformLayout();
-=======
 			this.button1 = new System.Windows.Forms.Button();
 			this.textBoxA = new System.Windows.Forms.TextBox();
 			this.labelA = new System.Windows.Forms.Label();
 			this.labelB = new System.Windows.Forms.Label();
-			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBoxB = new System.Windows.Forms.TextBox();
 			this.labelh = new System.Windows.Forms.Label();
 			this.textBoxH = new System.Windows.Forms.TextBox();
-			this.button2 = new System.Windows.Forms.Button();
+			this.textFunc = new System.Windows.Forms.TextBox();
 			this.label1 = new System.Windows.Forms.Label();
-			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBoxRec = new System.Windows.Forms.TextBox();
+			this.textBoxSim = new System.Windows.Forms.TextBox();
 			this.SuspendLayout();
 			// 
 			// button1
@@ -175,19 +46,20 @@
 			this.button1.BackColor = System.Drawing.Color.Black;
 			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
 			this.button1.ForeColor = System.Drawing.Color.Red;
-			this.button1.Location = new System.Drawing.Point(719, 151);
+			this.button1.Location = new System.Drawing.Point(575, 90);
 			this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(169, 78);
 			this.button1.TabIndex = 0;
 			this.button1.Text = "Count";
 			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
 			// textBoxA
 			// 
 			this.textBoxA.BackColor = System.Drawing.Color.Black;
 			this.textBoxA.ForeColor = System.Drawing.Color.Red;
-			this.textBoxA.Location = new System.Drawing.Point(300, 57);
+			this.textBoxA.Location = new System.Drawing.Point(300, 164);
 			this.textBoxA.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.textBoxA.Name = "textBoxA";
 			this.textBoxA.Size = new System.Drawing.Size(132, 22);
@@ -199,7 +71,7 @@
 			this.labelA.BackColor = System.Drawing.Color.Black;
 			this.labelA.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
 			this.labelA.ForeColor = System.Drawing.Color.Red;
-			this.labelA.Location = new System.Drawing.Point(103, 46);
+			this.labelA.Location = new System.Drawing.Point(81, 164);
 			this.labelA.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.labelA.Name = "labelA";
 			this.labelA.Size = new System.Drawing.Size(149, 52);
@@ -212,22 +84,22 @@
 			this.labelB.BackColor = System.Drawing.Color.Black;
 			this.labelB.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
 			this.labelB.ForeColor = System.Drawing.Color.Red;
-			this.labelB.Location = new System.Drawing.Point(103, 127);
+			this.labelB.Location = new System.Drawing.Point(103, 238);
 			this.labelB.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.labelB.Name = "labelB";
 			this.labelB.Size = new System.Drawing.Size(129, 52);
 			this.labelB.TabIndex = 3;
 			this.labelB.Text = "UP B";
 			// 
-			// textBox1
+			// textBoxB
 			// 
-			this.textBox1.BackColor = System.Drawing.Color.Black;
-			this.textBox1.ForeColor = System.Drawing.Color.Red;
-			this.textBox1.Location = new System.Drawing.Point(300, 127);
-			this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(132, 22);
-			this.textBox1.TabIndex = 4;
+			this.textBoxB.BackColor = System.Drawing.Color.Black;
+			this.textBoxB.ForeColor = System.Drawing.Color.Red;
+			this.textBoxB.Location = new System.Drawing.Point(300, 238);
+			this.textBoxB.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.textBoxB.Name = "textBoxB";
+			this.textBoxB.Size = new System.Drawing.Size(132, 22);
+			this.textBoxB.TabIndex = 4;
 			// 
 			// labelh
 			// 
@@ -235,7 +107,7 @@
 			this.labelh.BackColor = System.Drawing.Color.Black;
 			this.labelh.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
 			this.labelh.ForeColor = System.Drawing.Color.Red;
-			this.labelh.Location = new System.Drawing.Point(103, 222);
+			this.labelh.Location = new System.Drawing.Point(188, 304);
 			this.labelh.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.labelh.Name = "labelh";
 			this.labelh.Size = new System.Drawing.Size(48, 52);
@@ -246,44 +118,50 @@
 			// 
 			this.textBoxH.BackColor = System.Drawing.Color.Black;
 			this.textBoxH.ForeColor = System.Drawing.Color.Red;
-			this.textBoxH.Location = new System.Drawing.Point(300, 222);
+			this.textBoxH.Location = new System.Drawing.Point(300, 304);
 			this.textBoxH.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.textBoxH.Name = "textBoxH";
 			this.textBoxH.Size = new System.Drawing.Size(132, 22);
 			this.textBoxH.TabIndex = 6;
 			// 
-			// button2
+			// textFunc
 			// 
-			this.button2.BackColor = System.Drawing.Color.Black;
-			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.button2.ForeColor = System.Drawing.Color.Red;
-			this.button2.Location = new System.Drawing.Point(173, 318);
-			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(182, 59);
-			this.button2.TabIndex = 7;
-			this.button2.Text = "Enter";
-			this.button2.UseVisualStyleBackColor = false;
+			this.textFunc.BackColor = System.Drawing.Color.Black;
+			this.textFunc.ForeColor = System.Drawing.Color.Red;
+			this.textFunc.Location = new System.Drawing.Point(300, 90);
+			this.textFunc.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.textFunc.Name = "textFunc";
+			this.textFunc.Size = new System.Drawing.Size(132, 22);
+			this.textFunc.TabIndex = 7;
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
 			this.label1.BackColor = System.Drawing.Color.Black;
-			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
 			this.label1.ForeColor = System.Drawing.Color.Red;
-			this.label1.Location = new System.Drawing.Point(502, 318);
+			this.label1.Location = new System.Drawing.Point(64, 90);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(153, 52);
+			this.label1.Size = new System.Drawing.Size(159, 46);
 			this.label1.TabIndex = 8;
-			this.label1.Text = "Result";
+			this.label1.Text = "Fuction";
 			// 
-			// textBox2
+			// textBoxRec
 			// 
-			this.textBox2.BackColor = System.Drawing.Color.Black;
-			this.textBox2.ForeColor = System.Drawing.Color.Red;
-			this.textBox2.Location = new System.Drawing.Point(700, 332);
-			this.textBox2.Name = "textBox2";
-			this.textBox2.Size = new System.Drawing.Size(215, 22);
-			this.textBox2.TabIndex = 9;
+			this.textBoxRec.BackColor = System.Drawing.Color.Black;
+			this.textBoxRec.Location = new System.Drawing.Point(548, 238);
+			this.textBoxRec.Name = "textBoxRec";
+			this.textBoxRec.Size = new System.Drawing.Size(233, 22);
+			this.textBoxRec.TabIndex = 9;
+			// 
+			// textBoxSim
+			// 
+			this.textBoxSim.BackColor = System.Drawing.Color.Black;
+			this.textBoxSim.Location = new System.Drawing.Point(548, 303);
+			this.textBoxSim.Name = "textBoxSim";
+			this.textBoxSim.Size = new System.Drawing.Size(233, 22);
+			this.textBoxSim.TabIndex = 10;
 			// 
 			// Form1
 			// 
@@ -292,12 +170,13 @@
 			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
 			this.BackgroundImage = global::integralTest.Properties.Resources.IMG_2109;
 			this.ClientSize = new System.Drawing.Size(1064, 453);
-			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBoxSim);
+			this.Controls.Add(this.textBoxRec);
 			this.Controls.Add(this.label1);
-			this.Controls.Add(this.button2);
+			this.Controls.Add(this.textFunc);
 			this.Controls.Add(this.textBoxH);
 			this.Controls.Add(this.labelh);
-			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.textBoxB);
 			this.Controls.Add(this.labelB);
 			this.Controls.Add(this.labelA);
 			this.Controls.Add(this.textBoxA);
@@ -307,7 +186,6 @@
 			this.Text = "Tsvetkova, Shepeleva 3-46";
 			this.ResumeLayout(false);
 			this.PerformLayout();
->>>>>>> dd4b0f0f24d613e2d1bd29e88f39c976aedf9962
 
         }
 
@@ -323,7 +201,9 @@
 <<<<<<< HEAD
         private System.Windows.Forms.TextBox textFunc;
         private System.Windows.Forms.Label label1;
-    }
+		private System.Windows.Forms.TextBox textBoxRec;
+		private System.Windows.Forms.TextBox textBoxSim;
+	}
 =======
 		private System.Windows.Forms.Button button2;
 		private System.Windows.Forms.Label label1;
